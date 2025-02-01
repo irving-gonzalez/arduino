8219e626a4f1c4cc8ed610f90c94a3ff79779df1
Irving Gonzalez <irving_a_gonzalez@hotmail.com>
2025-01-31 19:32:11 -0600

    Update sketch_jan31a.ino

--- sketch_jan31a/sketch_jan31a.ino
+++ sketch_jan31a/sketch_jan31a.ino
@@ -1,27 +1,17 @@
-const int PIN_TO_SENSOR = 2;   // the pin that OUTPUT pin of sensor is connected to
-const int PIN_TO_LED = 13;   // the pin that OUTPUT pin of sensor is connected to
-int pinStateCurrent   = LOW; // current state of pin
-int pinStatePrevious  = LOW; // previous state of pin
+const int LED_PINS[] = {13, 12, 11, 10};  // Define 4 LED pins
 
 void setup() {
-  Serial.begin(9600);            // initialize serial
-  pinMode(PIN_TO_SENSOR, INPUT); // set arduino pin to input mode to read value from OUTPUT pin of sensor
-  pinMode(PIN_TO_LED, OUTPUT);
+  // Set all LED pins as OUTPUT
+  for (int i = 0; i < 4; i++) {
+    pinMode(LED_PINS[i], OUTPUT);
+  }
 }
 
-
 void loop() {
-  pinStatePrevious = pinStateCurrent; // store old state
-  pinStateCurrent = digitalRead(PIN_TO_SENSOR);   // read new state
+  // Turn ON all LEDs
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(LED_PINS[i], HIGH);
+  }
 
-  if (pinStatePrevious == LOW && pinStateCurrent == HIGH) {   // pin state change: LOW -> HIGH
-    Serial.println("Motion detected!");
-    digitalWrite(PIN_TO_LED, HIGH);
-    //mandar mensaje de texto aqui
-   
-  }
-  else if (pinStatePrevious == HIGH && pinStateCurrent == LOW) {   // pin state change: HIGH -> LOW
-    Serial.println("Motion stopped!");
-    digitalWrite(PIN_TO_LED, LOW);
-  }
+  // LEDs stay ON forever
 }
